cb59288e2c9e6479bf792e38dfd04415326e9080
jiejen <jiezhang0123@gmail.com>
2024-03-31 15:48:22 -0500

    update create.sql to include PK/FK constraints

--- create.sql
+++ create.sql
@@ -2,81 +2,100 @@ DROP DATABASE IF EXISTS FitQuest;
 CREATE DATABASE FitQuest;
 USE FitQuest;
 
-CREATE TABLE trainer_user (
-	trainer_id INT NOT NULL,
+CREATE TABLE sys_user (
+    user_id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50),
+    last_name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    PRIMARY KEY (trainer_id)
+    pass_word VARCHAR(50) NOT NULL,
+    CONSTRAINT userPK PRIMARY KEY (user_id),
+    CONSTRAINT userSK UNIQUE (email)
 );
 
 CREATE TABLE client_user (
-	client_id INT NOT NULL,
-    trainer_id INT NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50),
-    email VARCHAR(50) NOT NULL,
+    client_id INT NOT NULL,
+    trainer_id INT,
     weight FLOAT,
     height FLOAT,
-    PRIMARY KEY (client_id),
-    FOREIGN KEY (trainer_id) REFERENCES trainer_user(trainer_id)
+    CONSTRAINT clientPK PRIMARY KEY (client_id),
+    CONSTRAINT clientFK FOREIGN KEY (client_id)
+        REFERENCES sys_user (user_id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT trainerFK FOREIGN KEY (trainer_id)
+        REFERENCES sys_user (user_id)
+        ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 CREATE TABLE goal (
-	client_id INT NOT NULL,
-	goal_id INT NOT NULL,
+    client_id INT NOT NULL,
+    goal_id INT NOT NULL AUTO_INCREMENT,
     goal_description VARCHAR(100) NOT NULL,
     target_date DATE,
-    PRIMARY KEY (client_id, goal_id),
-    FOREIGN KEY (client_id) REFERENCES client_user(client_id)
+    CONSTRAINT goalPK PRIMARY KEY (goal_id),
+    CONSTRAINT goal_clientFK FOREIGN KEY (client_id)
+        REFERENCES client_user (client_id)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE food (
-	food_id INT NOT NULL,
-    calories INT NOT NULL,
+    food_id INT NOT NULL AUTO_INCREMENT,
+    calories INT NOT NULL DEFAULT 0,
     carbohydrates FLOAT,
     fat FLOAT,
     protein FLOAT,
-    PRIMARY KEY (food_id)
+    CONSTRAINT foodPK PRIMARY KEY (food_id)
 );
 
 CREATE TABLE food_log (
-	client_id INT NOT NULL,
-    food_log_id INT NOT NULL,
+    client_id INT NOT NULL,
+    food_log_id INT NOT NULL AUTO_INCREMENT,
     food_log_date DATE NOT NULL,
-    food_id INT NOT NULL,
-    PRIMARY KEY (client_id, food_log_id),
-    FOREIGN KEY (client_id) REFERENCES client_user(client_id),
-    FOREIGN KEY (food_id) REFERENCES food(food_id)
+    food_id INT,
+    servings INT NOT NULL DEFAULT 1,
+    CONSTRAINT food_logPK PRIMARY KEY (food_log_id),
+    CONSTRAINT food_log_clientFK FOREIGN KEY (client_id)
+        REFERENCES client_user (client_id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT food_logPK FOREIGN KEY (food_id)
+        REFERENCES food (food_id)
+        ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 CREATE TABLE exercise (
-	exercise_id INT NOT NULL,
-    PRIMARY KEY (exercise_id)
+    exercise_id INT NOT NULL AUTO_INCREMENT,
+    CONSTRAINT exercisePK PRIMARY KEY (exercise_id)
 );
 
 CREATE TABLE set_based_exercise (
-	exercise_id INT NOT NULL,
-    sets INT NOT NULL,
-    repetitions INT NOT NULL,
-    PRIMARY KEY (exercise_id),
-    FOREIGN KEY (exercise_id) REFERENCES exercise(exercise_id)
+    exercise_id INT NOT NULL,
+    sets INT NOT NULL DEFAULT 1,
+    repetitions INT NOT NULL DEFAULT 1,
+    CONSTRAINT set_based_exercisePK PRIMARY KEY (exercise_id),
+    CONSTRAINT set_based_exerciseFK FOREIGN KEY (exercise_id)
+        REFERENCES exercise (exercise_id)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE duration_based_exercise (
-	exercise_id INT NOT NULL,
-    duration_min FLOAT NOT NULL,
-    calories_burned INT,
-    PRIMARY KEY (exercise_id),
-    FOREIGN KEY (exercise_id) REFERENCES exercise(exercise_id)
+    exercise_id INT NOT NULL,
+    duration_min FLOAT NOT NULL DEFAULT 0,
+    calories_burned INT NOT NULL DEFAULT 0,
+    CONSTRAINT duration_based_exercisePK PRIMARY KEY (exercise_id),
+    CONSTRAINT duration_based_exerciseFK FOREIGN KEY (exercise_id)
+        REFERENCES exercise (exercise_id)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE exercise_log (
     client_id INT NOT NULL,
-    exercise_log_id INT NOT NULL,
+    exercise_log_id INT NOT NULL AUTO_INCREMENT,
     exercise_log_date DATE NOT NULL,
-    exercise_id INT NOT NULL,
-    PRIMARY KEY (client_id, exercise_log_id),
-    FOREIGN KEY (client_id) REFERENCES client_user(client_id),
-    FOREIGN KEY (exercise_id) REFERENCES exercise(exercise_id)
+    exercise_id INT,
+    CONSTRAINT exercise_logPK PRIMARY KEY (exercise_log_id),
+    CONSTRAINT exercise_log_clientFK FOREIGN KEY (client_id)
+        REFERENCES client_user (client_id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT exercise_log_exerciseFK FOREIGN KEY (exercise_id)
+        REFERENCES exercise (exercise_id)
+        ON DELETE SET NULL ON UPDATE CASCADE
 );
