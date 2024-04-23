8a2f566a4b6c6f5639a594693ae667fbc2821909
hxp210024 <143547254+hxp210024@users.noreply.github.com>
2024-04-23 01:04:37 -0500

    Update profile.php
    css updated

--- frontend/HTML_PHP/profile.php
+++ frontend/HTML_PHP/profile.php
@@ -38,13 +38,22 @@ mysqli_close($conn);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Profile - Fitness Tracking App</title>
-    <link rel="stylesheet" href="path/to/styles.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <header>
         <nav>
             <ul>
-                <li><a href="navigation.php">Home</a></li>
+            <li><a href="navigation.php">Home</a></li>
+                <li><a href="profile.php">Profile</a></li>
+                <li><a href="goal_setting.html">Set Goals</a></li>
+                <li><a href="exercise_log.php">Log Exercises</a></li>
+                <li><a href="meal_log.php">Log Meals</a></li>
+                <li><a href="view_goals.php">View Goals</a></li>
+                <li><a href="view_exercise_log.php">View Exercises</a></li>
+                <li><a href="view_meal_log.php">View Meals</a></li>
+                <li><a href="add_exercise.php">Custom Exercise</a></li>
+                <li><a href="add_meal.php">Custom Meal</a></li>
             </ul>
         </nav>
     </header>
