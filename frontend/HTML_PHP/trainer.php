f7932533104df94c31c77b7cf6c5a2b59bc32a7b
hxp210024 <143547254+hxp210024@users.noreply.github.com>
2024-04-23 18:54:18 -0500

    Add files via upload
    Basic Implementation completed -> will add more once the other features are added

---
+++ frontend/HTML_PHP/trainer.php
@@ -0,0 +1,39 @@
+<?php
+// Start the session
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fitness Tracking App</title>
+    <!-- You can include CSS files here if needed -->
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="view_clients.php">View Clients</a></li>
+                <li><a href="add_clients.php">Add Clients</a></li>
+                <li style="float:right"><a href="logout.php" >Log Out</a></li>
+            </ul>
+        </nav>
+
+        <main>
+        <section>
+            <h1>Welcome <?php echo $_SESSION['fname']; ?>! </h1>
+        </section>
+    </main>
+    </header>
+
+
+    <footer>
+        <p>&copy; Fitness Tracking App</p>
+    </footer>
+
+    <!-- You can include JavaScript files here if needed -->
+    <script src="path/to/scripts.js"></script>
+</body>
