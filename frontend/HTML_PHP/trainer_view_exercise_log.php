323996577003090fc937f5044fe9d6fe6eaa2e4c
hxp210024 <143547254+hxp210024@users.noreply.github.com>
2024-04-24 18:39:35 -0500

    Update trainer_view_exercise_log.php
    Navigation changed

--- frontend/HTML_PHP/trainer_view_exercise_log.php
+++ frontend/HTML_PHP/trainer_view_exercise_log.php
@@ -49,6 +49,7 @@ $result = mysqli_query($conn, $fetchsql);
                 <li><a href="trainer.php">Home</a></li>
                 <li><a href="view_clients.php">View Clients</a></li>
                 <li><a href="add_clients.php">Add Clients</a></li>
+                <li><a href="remove_client.php">Remove Clients</a></li>
                 <li><a href="trainer_view_meal_log.php">Client meal logs</a></li>
                 <li><a href="trainer_view_exercise_log.php">Client exercise logs</a></li>
                 <li style="float:right"><a href="logout.php" >Log Out</a></li>
