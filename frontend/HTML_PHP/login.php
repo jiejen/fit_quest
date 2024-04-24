c66bead3bada7b9ff8b0271c6b8d871cdb5e4620
hxp210024 <143547254+hxp210024@users.noreply.github.com>
2024-04-23 19:19:31 -0500

    Update login.php
    Added prepared statements

--- frontend/HTML_PHP/login.php
+++ frontend/HTML_PHP/login.php
@@ -1,46 +1,63 @@
 <?php
-    session_start();
+session_start();
 
-    $db_server = "localhost";
-    $db_user = "root";
-    $db_pass = "";
-    $db_name = "fitquest";
-    $conn = "";
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "fitquest";
+$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
 
-    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['email']) && isset($_POST['password'])) {
     $email = $_POST["email"];
     $password = $_POST["password"];
-    
-    $loginsql = "SELECT * FROM login_info WHERE email = '$email' AND pass_word = '$password'
-    ";
-    $result = mysqli_query($conn, $loginsql);
 
-    if (mysqli_num_rows($result) == 1) {
-        // User exists, redirect to home page or perform further actions
-        $fnamesql = "SELECT first_name, last_name, user_id FROM sys_user WHERE email = '$email'";
-        $fnameresult = mysqli_query($conn, $fnamesql);
-        if (mysqli_num_rows($fnameresult) > 0){
-            $row = mysqli_fetch_assoc($fnameresult);
+    $loginsql = "SELECT * FROM login_info WHERE email = ? AND pass_word = ?";
+    $stmt = $conn->prepare($loginsql);
+    $stmt->bind_param("ss", $email, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows == 1) {
+        $fnamesql = "SELECT first_name, last_name, user_id FROM sys_user WHERE email = ?";
+        $fnamestmt = $conn->prepare($fnamesql);
+        $fnamestmt->bind_param("s", $email);
+        $fnamestmt->execute();
+        $fnameresult = $fnamestmt->get_result();
+        if ($fnameresult->num_rows > 0){
+            $row = $fnameresult->fetch_assoc();
             $id = $row['user_id'];
             $_SESSION['fname'] = $row['first_name'];
             $_SESSION['lname'] = $row['last_name'];
             $_SESSION['user_id'] = $id;
+            
+            $clientsql = "SELECT trainer_id, height, weight FROM client_user WHERE client_id = ?";
+            $clientstmt = $conn->prepare($clientsql);
+            $clientstmt->bind_param("i", $id);
+            $clientstmt->execute();
+            $clientresult = $clientstmt->get_result();
+            if ($clientresult->num_rows > 0) {
+                $row = $clientresult->fetch_assoc();
+                $_SESSION['isClient'] = true;
+                $_SESSION['trainer_id'] = $row['trainer_id'];
+                $_SESSION['height'] = $row['height'];
+                $_SESSION['weight'] = $row['weight'];
+            }
         }
-        $clientsql = "SELECT trainer_id, height, weight FROM client_user WHERE client_id = '$id'";
-        $clientresult = mysqli_query($conn, $clientsql);
-        if (mysqli_num_rows($clientresult) > 0) {
-            $row = mysqli_fetch_assoc($clientresult);
-            $_SESSION['isClient'] = true;
-            $_SESSION['trainer_id']= $row['trainer_id'];
-            $_SESSION['height'] = $row['height'];
-            $_SESSION['weight'] = $row['weight'];
-        }
+        $fnamestmt->close();
+        $clientstmt->close();
         header("Location: navigation.php");
     } else {
         header("Location: login.html?error=invalid_credentials");
     }
+    $stmt->close();
+} else {
+    header("Location: login.html?error=invalid_request");
+}
 
-    mysqli_close($conn);
-    exit();
+mysqli_close($conn);
+exit();
 ?>
