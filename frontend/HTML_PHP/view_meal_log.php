a5fe0d153c34094d86b702c5a423de1e1c78895c
hxp210024 <143547254+hxp210024@users.noreply.github.com>
2024-04-25 16:03:16 -0500

    Update view_meal_log.php
    Delete feature added

--- frontend/HTML_PHP/view_meal_log.php
+++ frontend/HTML_PHP/view_meal_log.php
@@ -6,6 +6,7 @@
     $db_pass = "";
     $db_name = "fitquest";
     $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    
 ?>
 
 <!DOCTYPE html>
@@ -49,7 +50,7 @@
             if (mysqli_num_rows($result) > 0){
                 // Output table header
                 echo "<table border='1' style='width: 50%; border-collapse: collapse;'>";
-                echo "<tr><th>Date</th><th>Food</th><th>Servings</th><th>Calories</th><th>Carbs</th><th>Fats</th><th>Proteins</th></tr>";
+                echo "<tr><th>Date</th><th>Food</th><th>Servings</th><th>Calories</th><th>Carbs</th><th>Fats</th><th>Proteins</th><th>Action</th></tr>";
 
                 // Loop through each meal log record
                 while ($row = mysqli_fetch_assoc($result)) {
@@ -66,8 +67,7 @@
                         $food_protein = $foodrow['protein'];
                     }
 
-                    // Output table rows with meal log details
-                    echo "<tr style='background-color: #f2f2f2;'>";
+                    echo "<tr style='background-color: #f2f2f2; height: 50px'>";
                     echo "<td style='padding: 8px; text-align: center;'>" . $row['food_log_date'] . "</td>";
                     echo "<td style='padding: 8px; text-align: center;'>" . $food_name . "</td>";
                     echo "<td style='padding: 8px; text-align: center;'>" . $row['servings'] . "</td>";
@@ -75,7 +75,9 @@
                     echo "<td style='padding: 8px; text-align: center;'>" . $food_carbs . "</td>";
                     echo "<td style='padding: 8px; text-align: center;'>" . $food_fat. "</td>";
                     echo "<td style='padding: 8px; text-align: center;'>" . $food_protein. "</td>";
+                    echo "<td style='padding: 8px; text-align: center;'><form action='delete_meal.php' method='POST'><input type='hidden' name='food_log_id' value='" . $row['food_log_id'] . "'><input type='submit' value='Remove' onclick='return confirm(\"Are you sure you want to remove this meal?\");'></form></td>";
                     echo "</tr>";
+
                 }
 
                 // Close the table
